c7578ce1cad1e368d41a4419bf67050ef5b178eb
praveen <otty.praveen@gmail.com>
2022-06-30 23:20:28 +0530

    this is from 2nd guy

--- src/test/java/GITPractise_MainProject/TestCase1.java
+++ src/test/java/GITPractise_MainProject/TestCase1.java
@@ -20,6 +20,7 @@ public class TestCase1 {
 		System.out.println("This is 4th TestCase of module 1");
 		System.out.println("This is 5th TestCase of module 1");
 		System.out.println("This is 6th TestCase of module 1");
+		System.out.println("This is 7th TestCase of module 1");
 	}
 	
 
