b649640b96193a98a1998f19d7c91a784002e363
praveen <otty.praveen@gmail.com>
2022-06-30 22:44:35 +0530

    This is my first Comment

---
+++ src/main/java/GITPractise_MainProject/App.java
@@ -0,0 +1,13 @@
+package GITPractise_MainProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GITPractise_MainProject/TestCase1.java
@@ -0,0 +1,23 @@
+package GITPractise_MainProject;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+	
+	@Test
+	public void getTestCase1() 
+	
+	{
+		
+		System.out.println("This is 1st TestCase of module 1");
+		System.out.println("This is 2nd TestCase of module 1");
+	}
+	
+	@Test
+	public void gettestCase1subSet()
+	{
+		System.out.println("This is 3rd TestCase of module 1");
+	}
+	
+
+}
