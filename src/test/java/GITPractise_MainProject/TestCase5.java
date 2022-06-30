cc8f1cfed060fbc0396d4a3b59520f0cf7b4f0b8
praveen <otty.praveen@gmail.com>
2022-06-30 23:54:52 +0530

    changes made

---
+++ src/test/java/GITPractise_MainProject/TestCase5.java
@@ -0,0 +1,54 @@
+package GITPractise_MainProject;
+
+import org.testng.annotations.Test;
+
+public class TestCase5 {
+	
+	@Test
+	public void getTestCase1() 
+	
+	{
+		
+		System.out.println("This is 1st TestCase of module 1");
+		System.out.println("This is 2nd TestCase of module 1");
+	}
+	
+	@Test
+	public void gettestCase1subSet()
+	{
+		System.out.println("This is 3rd TestCase of module 1");
+		System.out.println("This is 4th TestCase of module 1");
+		System.out.println("This is 5th TestCase of module 1");
+		System.out.println("This is 6th TestCase of module 1");
+		System.out.println("This is 7th TestCase of module 1");
+		System.out.println("This is 8th TestCase of module 1");
+		System.out.println("This is 9th TestCase of module 1");
+	}
+	
+	@Test
+	public void gettestCase3subSet()
+	{
+		System.out.println("This is 10th TestCase of module 1");
+		System.out.println("This is 11th TestCase of module 1");
+		System.out.println("This is 12th TestCase of module 1");
+		System.out.println("This is 13th TestCase of module 1");
+		System.out.println("This is 14th TestCase of module 1");
+		System.out.println("This is 15th TestCase of module 1");
+		System.out.println("This is 16th TestCase of module 1");
+	}
+	
+	@Test
+	public void gettestCase4subSet()
+	{
+		System.out.println("This is 10th TestCase of module 1");
+		System.out.println("This is 11th TestCase of module 1");
+		System.out.println("This is 12th TestCase of module 1");
+		System.out.println("This is 13th TestCase of module 1");
+		System.out.println("This is 14th TestCase of module 1");
+		System.out.println("This is 15th TestCase of module 1");
+		System.out.println("This is 16th TestCase of module 1");
+	}
+	
+	
+
+}
---
+++ src/test/java/package2/classA.java
@@ -0,0 +1,15 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class classA {
+	@Test
+	public void getTestCase2() 
+	
+	{
+		
+		System.out.println("This is 1st TestCase of module 1");
+		System.out.println("This is 2nd TestCase of module 1");
+	}
+
+}
