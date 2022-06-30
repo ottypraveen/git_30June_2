013cba155ddd06b47799d4f598cff931adbba3f8
praveen <otty.praveen@gmail.com>
2022-06-30 23:23:11 +0530

    test case is changed

--- src/test/java/GITPractise_MainProject/TestCase1.java
+++ src/test/java/GITPractise_MainProject/TestCase1.java
@@ -21,6 +21,7 @@ public class TestCase1 {
 		System.out.println("This is 5th TestCase of module 1");
 		System.out.println("This is 6th TestCase of module 1");
 		System.out.println("This is 7th TestCase of module 1");
+		System.out.println("This is 8th TestCase of module 1");
 	}
 	
 
